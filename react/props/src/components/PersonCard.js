d410a22036a2e9b8409f0a2da77be6a2698887f3
schuang25 <stephen.chuang25@gmail.com>
2022-03-10 09:16:20 -0800

    putting it together assignment

--- react/props/src/components/PersonCard.js
+++ react/props/src/components/PersonCard.js
@@ -1,15 +1,28 @@
 import React, {Component} from 'react';
 
 class PersonCard extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            age: props.age
+        };
+    }
+
+    birthday = () => {
+        let newAge = this.state.age + 1;
+        this.setState({age: newAge});
+    }
+
     render() {
         return(
             <div>
                 <h2>{this.props.lastName}, {this.props.firstName}</h2>
-                <p>Age: {this.props.age}</p>
+                <p>Age: {this.state.age}</p>
                 <p>Hair Color: {this.props.hairColor}</p>
+                <button onClick={this.birthday}>Birthday button for {this.props.firstName} {this.props.lastName}</button>
             </div>
         )
-    }
+    }  
 }
 
 export default PersonCard;
