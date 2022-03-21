9f78e2e8b88ed14f3628bae18691b1fa2c2c09eb
schuang25 <stephen.chuang25@gmail.com>
2022-03-21 10:22:02 -0700

    w3d1 algos

---
+++ algos/w3d1.js
@@ -0,0 +1,48 @@
+// findObjectsFilter(searchFor, itemsArr)
+
+// given a 'search for' object with primitive values and a list of objects
+// return a new list of objects containing the same key value pairs as the search for
+
+// given searchFor and items
+const items = [
+    { firstName: "Bob", lastName: "Robert", age: 31 },
+    { firstName: "John", lastName: "Smith", age: 25 },
+    { firstName: "Bob", lastName: "White", age: 31 },
+    { firstName: "Bob", lastName: "Smith", age: 27 },
+];
+
+const searchFor1 = {
+    firstName: "Bob",
+    age: 31
+};
+// return a new list of objects containing the same key pair values
+const output1 = [
+    { firstName: "Bob", lastName: "Robert", age: 31 },
+    { firstName: "Bob", lastName: "White", age: 31 },
+    //   { firstName: "Bob", lastName: "Smith", age: 27 },
+];
+
+const searchFor2 = {
+    lastName: "Smith",
+};
+const output2 = [
+    { firstName: "John", lastName: "Smith", age: 25 },
+    { firstName: "Bob", lastName: "Smith", age: 27 },
+];
+
+function findObjectsFilter(searchObj, items) {
+    let output = [];
+    for (obj of items) {
+        let match = true;
+        for (key in searchObj) {
+            if (searchObj[key] !== obj[key])
+                match = false;
+        }
+        if (match)
+            output.push(obj);
+    }
+    return output;
+}
+
+console.log(findObjectsFilter(searchFor1, items));
+console.log(findObjectsFilter(searchFor2, items));
