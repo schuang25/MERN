d7666f3c002c0aeb0a48945813452a6f4d092cea
schuang25 <stephen.chuang25@gmail.com>
2022-03-24 09:53:34 -0700

    w3d4 algos

---
+++ algos/w3d4.js
@@ -0,0 +1,96 @@
+/*
+  Given an array of ailments (illnesses), and an array of medication objects that have a nested array of treatedSymptoms
+  return the medication name(s) that treats the most given symptoms
+*/
+
+const medications = [
+    {
+        name: "Sulforaphane",
+        treatableSymptoms: [
+            "dementia",
+            "alzheimer's",
+            "inflammation",
+            "neuropathy",
+        ],
+    },
+    {
+        
+        name: "Longvida Curcumin",
+        treatableSymptoms: [
+            "pain",
+            "inflammation",
+            "depression",
+            "arthritis",
+            "anxiety",
+        ],
+    },
+    {
+        name: "Hericium erinaceus",
+        treatableSymptoms: [
+            "anxiety",
+            "cognitive decline",
+            "depression"],
+    },
+    {
+        name: "Nicotinamide mononucleotide",
+        treatableSymptoms: [
+            "ageing",
+            "low NAD",
+            "obesity",
+            "mitochondrial myopathy",
+            "diabetes",
+        ],
+    },
+    {
+        name: "PainAssassinator",
+        treatableSymptoms: [
+            "pain",
+            "inflammation",
+            "cramps",
+            "headache",
+            "toothache",
+            "back pain",
+            "fever",
+        ],
+    },
+];
+
+
+/*
+Input: ["pain"], medications
+Output: ["PainAssassinator", "Longvida Curcumin"]
+*/
+
+/*
+Input: ["pain", "inflammation", "depression"], medications
+Output: ["Longvida Curcumin"]
+*/
+
+/*
+Input: ["existential dread"], medications
+Output: []
+*/
+
+function webMD(ailments, meds) {
+    let maxSymptoms = 1;
+    let output = [];
+    for (const med of meds) {
+        let treatedSymptoms = 0;
+        for (const ailment of ailments) {
+            if (med.treatableSymptoms.includes(ailment))
+                treatedSymptoms++;
+        }
+        if (treatedSymptoms === maxSymptoms)
+            output.push(med.name);
+        else if (treatedSymptoms > maxSymptoms) {
+            output = [];
+            output.push(med.name);
+            maxSymptoms = treatedSymptoms;
+        }
+    }
+    return output;
+}
+
+console.log(webMD(["pain"], medications));
+console.log(webMD(["pain", "inflammation", "depression"], medications));
+console.log(webMD(["existential dread"], medications));
